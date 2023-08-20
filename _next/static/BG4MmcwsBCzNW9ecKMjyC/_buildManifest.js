4e06de839b7aa37ab5e3e10fb0f5348556f4a5a8
thiagombatista <>
2023-08-20 07:16:21 +0000

    Updates

---
+++ _next/static/BG4MmcwsBCzNW9ecKMjyC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/868-e99b16b798340c8a.js","static/chunks/pages/index-0b2a1c512e9897d2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
